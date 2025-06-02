703ffac582defebff647bde06597786a3a7d9c8a
KeithEsc <keithesc@gmail.com>
2025-06-02 11:56:50 -0500

    updated colors and some stylings

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,35 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class', // This enables class-based dark mode
-	theme: {
-		extend: {
-			animation: {
-				gradient: 'gradient 1s linear infinite',
-			},
-			keyframes: {
-				gradient: {
-					'0%': { backgroundPosition: '0% 50%' },
-					'100%': { backgroundPosition: '100% 50%' },
-				},
-			},
-			cursor: {
-				'tennessee': 'url(/images/tennessee-outline.png), auto',
-			  },
-		},
-	},
-	plugins: [
-		require('tailwindcss/plugin')(function({ addUtilities }) {
-			addUtilities({
-				'.animate-gradient': {
-					backgroundSize: '200% 200%',
-					backgroundClip: 'text',
-					'-webkit-background-clip': 'text',
-				},
-			});
-		}),
-	],
-	fontFamily: {
-		serif: ['Georgia'],
-	},
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class", // This enables class-based dark mode
+  theme: {
+    extend: {
+      colors: {
+        light: "#fff9f9",
+        dark: "#3c3a39",
+        blue: "#00a4f3",
+      },
+      animation: {
+        gradient: "gradient 1s linear infinite",
+      },
+      keyframes: {
+        gradient: {
+          "0%": { backgroundPosition: "0% 50%" },
+          "100%": { backgroundPosition: "100% 50%" },
+        },
+      },
+      cursor: {
+        tennessee: "url(/images/tennessee-outline.png), auto",
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss/plugin")(function ({ addUtilities }) {
+      addUtilities({
+        ".animate-gradient": {
+          backgroundSize: "200% 200%",
+          backgroundClip: "text",
+          "-webkit-background-clip": "text",
+        },
+      });
+    }),
+  ],
+  fontFamily: {
+    serif: ["Georgia"],
+  },
 };
